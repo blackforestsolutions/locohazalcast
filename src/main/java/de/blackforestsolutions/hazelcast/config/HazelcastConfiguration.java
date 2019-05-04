b27c6925d609f780acc5224b17fe5729b0544760
mburkertt <matthiasburkerttriberg@googlemail.com>
2019-05-04 12:21:05 +0200

    Small fixes runnable

--- src/main/java/de/blackforestsolutions/hazelcast/config/HazelcastConfiguration.java
+++ src/main/java/de/blackforestsolutions/hazelcast/config/HazelcastConfiguration.java
@@ -1,16 +1,16 @@
 package de.blackforestsolutions.hazelcast.config;
 
-import com.hazelcast.config.EvictionPolicy;
-import com.hazelcast.config.MapConfig;
-import com.hazelcast.config.MaxSizeConfig;
+import com.hazelcast.config.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.hazelcast.config.Config;
 
 @Configuration
 public class HazelcastConfiguration {
 
     private static final int HAZELCASTSIZE = 200;
+    private static final int HAZELCASTMANAGEMENTCENTERUPDATEINTERVAL = 3;
+
+
 
     @Bean
     public Config hazelCastConfig() {
@@ -24,4 +24,7 @@ public class HazelcastConfiguration {
                                 .setTimeToLiveSeconds(-1));
         return config;
     }
+
+
+
 }
