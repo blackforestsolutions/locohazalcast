5983d8c8b91721201b6d4f564c76de132bad9eb9
mburkertt <matthiasburkerttriberg@googlemail.com>
2020-04-07 17:23:47 +0200

    Add deployment.sh to automate the docker deployment.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 ARG JAR_FILE
-#COPY ${JAR_FILE} myapp.jar
 ADD target/${JAR_FILE} myapp.jar
 RUN sh -c 'touch /myapp.jar'
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/myapp.jar"]
+ARG SOURCE_COMMIT
+ENV SOURCE_COMMIT $SOURCE_COMMIT
--- deployment.sh
+++ deployment.sh
@@ -3,11 +3,12 @@
 DOCKERFILE_PATH=Dockerfile
 IMAGE_NAME=locohazalcast
 DOCKER_REPO=blackforestsolutions
+DOCKER_TAG=0.0.1
 
-docker login --username ${USERNAME} --password ${PASSWORD}
+docker login
 
-docker build --build-arg DOCKER_TAG=$SOURCE_COMMIT -f $DOCKERFILE_PATH -t $IMAGE_NAME .
+docker build --build-arg DOCKER_TAG=0.0.1 -f $DOCKERFILE_PATH -t $IMAGE_NAME .
 
-docker tag $SOURCE_COMMIT $DOCKER_REPO/$IMAGE_NAME:$SOURCE_COMMIT
+docker tag $SOURCE_COMMIT $DOCKER_REPO/$IMAGE_NAME:0.0.1
 
-docker push $DOCKER_REPO/$IMAGE_NAME:$SOURCE_COMMIT
+docker push $DOCKER_REPO/$IMAGE_NAME:0.0.1
