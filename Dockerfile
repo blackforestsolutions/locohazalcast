989c60618b48ba36c41e01e1d13fec8d296a7fa0
mburkertt <matthiasburkerttriberg@googlemail.com>
2020-04-13 16:24:47 +0200

    Add Autodeploy

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,3 @@ ARG JAR_FILE
 ADD target/${JAR_FILE} myapp.jar
 RUN sh -c 'touch /myapp.jar'
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/myapp.jar"]
-ARG SOURCE_COMMIT
-ENV SOURCE_COMMIT $SOURCE_COMMIT
--- deployment.sh
+++ deployment.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+DOCKER_TAG=$1
 DOCKERFILE_PATH=Dockerfile
-IMAGE_NAME=locohazalcast
+IMAGE_NAME=locohazelcast
 DOCKER_REPO=blackforestsolutions
-DOCKER_TAG=0.0.1
+targetImage=$DOCKER_REPO/$IMAGE_NAME
+targetImageTagged=$targetImage:$DOCKER_TAG
+
+echo "docker tag is $DOCKER_TAG"
 
 docker login
 
-docker build --build-arg DOCKER_TAG=0.0.1 -f $DOCKERFILE_PATH -t $IMAGE_NAME .
+docker build --build-arg DOCKER_TAG=$DOCKER_TAG -f $DOCKERFILE_PATH -t $IMAGE_NAME .
 
-docker tag $SOURCE_COMMIT $DOCKER_REPO/$IMAGE_NAME:0.0.1
+sourceId=$(docker images --filter=reference=IMAGE_NAME --format "{{.ID}}")
 
-docker push $DOCKER_REPO/$IMAGE_NAME:0.0.1
+docker tag $sourceId $targetImageTagged
+
+docker push $targetImageTagged
